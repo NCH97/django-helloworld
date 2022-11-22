48250108153d69922f44c6ae6387a8f3cbf70206
NCH97 <Blingyb.chnn@gmaul.com>
2022-11-22 10:07:13 +0100

    changes error

--- HelloWorld_App/views.py
+++ HelloWorld_App/views.py
@@ -4,11 +4,11 @@ from django.http import HttpResponse
 # Create your views here.
 def sayHello(request):
     msg = '<h1 style="text-align: center">Hello, World!</h1'
-    msg += 'h2 style="text-align: center">Hello, World!</h2>'
-    msg += 'h3 style="text-align: center">Hello, World!</h3>'
-    msg += 'h4 style="text-align: center">Hello, World!</h4>'
-    msg += 'h5 style="text-align: center">Hello, World!</h5>'
-    msg += 'h6 style="text-align: center">Hello, World!</h6>'
-    msg += 'br/><br/><h2 style="text-align: center">Hello, < aan style="color:blue;">Christopher :-)</span></h2>'
-    return HttpResponse(msg1)
+    msg += '<h2 style="text-align: center">Hello, World!</h2>'
+    msg += '<h3 style="text-align: center">Hello, World!</h3>'
+    msg += '<h4 style="text-align: center">Hello, World!</h4>'
+    msg += '<h5 style="text-align: center">Hello, World!</h5>'
+    msg += '<h6 style="text-align: center">Hello, World!</h6>'
+    msg += '<br/><br/><h2 style="text-align: center">Hello, <span style="color:blue;">Christopher :-)</span></h2>'
+    return HttpResponse(msg)
 # Create your views here.
